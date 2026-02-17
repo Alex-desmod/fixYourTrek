3e2741d4c46c80ee12a6da032afcdde8a84a12f1
Alex Gusev <alex.desmod@gmail.com>
2026-02-17 20:02:52 +0300

    almost almost done)

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, FileResponse
+from starlette.staticfiles import StaticFiles
 
 from backend.routers import track
 
@@ -14,6 +15,8 @@ app.include_router(track.router)
 BASE_DIR = Path(__file__).resolve().parent.parent
 FRONTEND_DIR = BASE_DIR / "frontend"
 
+app.mount("/assets", StaticFiles(directory="frontend/dist/assets"), name="assets")
+
 @app.get("/", response_class=HTMLResponse)
-def root():
-    return FileResponse(FRONTEND_DIR / "index.html")
+def frontend():
+    return FileResponse(FRONTEND_DIR / "dist/index.html")
